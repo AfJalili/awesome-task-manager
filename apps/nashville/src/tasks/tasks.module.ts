a17319fe7984544ff7ea9a9146d5f82e462665bc
Afshin Jalili <a.jalili@kaman.ir>
2023-08-03 20:23:59 +0330

    fix(): typo

--- apps/nashville/src/tasks/tasks.module.ts
+++ apps/nashville/src/tasks/tasks.module.ts
@@ -1,10 +1,23 @@
-import {Module} from '@nestjs/common';
-import {TasksService} from './tasks.service';
-import {TasksController} from './tasks.controller';
+import { Module } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { TasksController } from './tasks.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { TASK_PACKAGE_NAME } from '@gallatin/interfaces/grpc';
 
 @Module({
-    controllers: [TasksController],
-    providers: [TasksService]
+  imports: [
+    ClientsModule.register([
+      {
+        name: TASK_PACKAGE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          package: TASK_PACKAGE_NAME,
+          protoPath: 'proto/tasks.proto',
+        },
+      },
+    ]),
+  ],
+  controllers: [TasksController],
+  providers: [TasksService],
 })
-export class TasksModule {
-}
+export class TasksModule {}
