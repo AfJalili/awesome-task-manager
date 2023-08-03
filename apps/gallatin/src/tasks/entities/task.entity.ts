30a3894690fd4e9581435bbfb380581cfd640f92
Afshin Jalili <a.jalili@kaman.ir>
2023-08-03 14:50:44 +0330

    feat(gallatin): decorate task entity with typeorm

--- apps/gallatin/src/tasks/entities/task.entity.ts
+++ apps/gallatin/src/tasks/entities/task.entity.ts
@@ -1,2 +1,22 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
 export class Task {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: true, default: null })
+  parentId: string | null = null;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
