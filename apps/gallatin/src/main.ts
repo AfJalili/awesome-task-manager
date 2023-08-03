07b4716e2d0f2be1f3f6ded2b515009bddc707aa
Afshin Jalili <a.jalili@kaman.ir>
2023-08-03 17:47:57 +0330

    feat(gallatin): setup service as grpc

--- apps/gallatin/src/main.ts
+++ apps/gallatin/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { GallatinModule } from './gallatin.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { TasksModule } from './tasks/tasks.module';
+import { join } from 'path';
+import { TASK_PACKAGE_NAME } from './proto/tasks';
 
 async function bootstrap() {
-  const app = await NestFactory.create(GallatinModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(TasksModule, {
+    transport: Transport.GRPC,
+    options: {
+      protoPath: join(__dirname, './proto/tasks.proto'),
+      package: TASK_PACKAGE_NAME,
+    },
+  });
+
+  await app.listen();
 }
+
 bootstrap();
