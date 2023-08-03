2c7ffc2e36d464d914e1513fb838d61f36f420b3
Afshin Jalili <a.jalili@kaman.ir>
2023-08-03 12:03:09 +0330

    feat(): add validation pipe

--- apps/nashville/src/main.ts
+++ apps/nashville/src/main.ts
@@ -1,16 +1,23 @@
 import {NestFactory} from '@nestjs/core';
 import {NashvilleModule} from './nashville.module';
-import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+import {HttpStatus, ValidationPipe} from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(NashvilleModule);
 
-  const config = new DocumentBuilder()
-      .setTitle('Awesome Task Manager')
-      .setDescription('The Awesome Task Manager Project APIs Specs.')
-      .build();
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            errorHttpStatusCode: HttpStatus.BAD_REQUEST,
+        })
+    );
 
-  const document = SwaggerModule.createDocument(app, config);
+    const documentOptions = new DocumentBuilder()
+        .setTitle('Awesome Task Manager')
+        .setDescription('The Awesome Task Manager Project APIs Specs.')
+        .build();
+    const document = SwaggerModule.createDocument(app, documentOptions);
   SwaggerModule.setup('docs/api/v1', app, document);
 
   await app.listen(3000);
