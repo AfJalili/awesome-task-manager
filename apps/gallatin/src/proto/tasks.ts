06e02e223699b2bb4f246a955b687e21dd6cc5dc
Afshin Jalili <a.jalili@kaman.ir>
2023-08-03 13:02:53 +0330

    feat(gallatin): generate ts file definition for tasks.proto

---
+++ apps/gallatin/src/proto/tasks.ts
@@ -0,0 +1,90 @@
+/* eslint-disable */
+import {GrpcMethod, GrpcStreamMethod} from "@nestjs/microservices";
+import {Observable} from "rxjs";
+
+export const protobufPackage = "tasks";
+
+export interface CreateTaskRequest {
+  parentId?: string | undefined;
+  title: string;
+  description: string;
+}
+
+export interface ReadTaskRequest {
+  id: string;
+}
+
+export interface UpdateTaskRequest {
+  id: string;
+  parentId?: string | undefined;
+  title?: string | undefined;
+  description?: string | undefined;
+}
+
+export interface DeleteTaskRequest {
+  id: string;
+}
+
+export interface ListTasksRequest {
+  page: number;
+  limit: number;
+}
+
+export interface ListTasksResponse {
+  tasks: Task[];
+}
+
+export interface Task {
+  id: string;
+  parentId?: string | undefined;
+  title: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Empty {
+}
+
+export const TASKS_PACKAGE_NAME = "tasks";
+
+export interface TasksServiceClient {
+  createTask(request: CreateTaskRequest): Observable<Task>;
+
+  readTask(request: ReadTaskRequest): Observable<Task>;
+
+  updateTask(request: UpdateTaskRequest): Observable<Task>;
+
+  deleteTask(request: DeleteTaskRequest): Observable<Task>;
+
+  listTasks(request: ListTasksRequest): Observable<ListTasksResponse>;
+}
+
+export interface TasksServiceController {
+  createTask(request: CreateTaskRequest): Promise<Task> | Observable<Task> | Task;
+
+  readTask(request: ReadTaskRequest): Promise<Task> | Observable<Task> | Task;
+
+  updateTask(request: UpdateTaskRequest): Promise<Task> | Observable<Task> | Task;
+
+  deleteTask(request: DeleteTaskRequest): Promise<Task> | Observable<Task> | Task;
+
+  listTasks(request: ListTasksRequest): Promise<ListTasksResponse> | Observable<ListTasksResponse> | ListTasksResponse;
+}
+
+export function TasksServiceControllerMethods() {
+  return function (constructor: Function) {
+    const grpcMethods: string[] = ["createTask", "readTask", "updateTask", "deleteTask", "listTasks"];
+    for (const method of grpcMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcMethod("TasksService", method)(constructor.prototype[method], method, descriptor);
+    }
+    const grpcStreamMethods: string[] = [];
+    for (const method of grpcStreamMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(constructor.prototype, method);
+      GrpcStreamMethod("TasksService", method)(constructor.prototype[method], method, descriptor);
+    }
+  };
+}
+
+export const TASKS_SERVICE_NAME = "TasksService";
