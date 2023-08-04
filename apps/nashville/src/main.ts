ae45a86e17c67a04584f84b6a90661db4259029d
Afshin Jalili <a.jalili@kaman.ir>
2023-08-04 13:37:03 +0330

    feat(nashville): enable cors

--- apps/nashville/src/main.ts
+++ apps/nashville/src/main.ts
@@ -1,25 +1,26 @@
-import {NestFactory} from '@nestjs/core';
-import {NashvilleModule} from './nashville.module';
-import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
-import {HttpStatus, ValidationPipe} from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { NashvilleModule } from "./nashville.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { HttpStatus, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(NashvilleModule);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      errorHttpStatusCode: HttpStatus.BAD_REQUEST,
+    })
+  );
 
-    app.useGlobalPipes(
-        new ValidationPipe({
-            transform: true,
-            errorHttpStatusCode: HttpStatus.BAD_REQUEST,
-        })
-    );
-
-    const documentOptions = new DocumentBuilder()
-        .setTitle('Awesome Task Manager')
-        .setDescription('The Awesome Task Manager Project APIs Specs.')
-        .build();
-    const document = SwaggerModule.createDocument(app, documentOptions);
+  const documentOptions = new DocumentBuilder()
+    .setTitle"Awesome Task Manager"')
+    .setDescription"The Awesome Task Manager Project APIs Specs."')
+    .build();
+  const document = SwaggerModule.createDocument(app, documentOptions);
   SwaggerModule.setup('docs/api/v1', app, document);
 
   await app.listen(3000);
 }
+
 bootstrap();
