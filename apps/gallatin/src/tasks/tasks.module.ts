81c72a0d5ef74e1ba64e9f555abacd21cc46be2a
Afshin Jalili <a.jalili@kaman.ir>
2023-08-04 13:41:38 +0330

    fix(nashville): syntax error

--- apps/gallatin/src/tasks/tasks.module.ts
+++ apps/gallatin/src/tasks/tasks.module.ts
@@ -1,10 +1,23 @@
-import {Module} from '@nestjs/common';
-import {TasksService} from './tasks.service';
-import {TasksController} from './tasks.controller';
+import { Module } from '@nestjs/common';
+import { TasksController } from './tasks.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './entities/task.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Task]),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'root',
+      entities: [Task],
+      synchronize: true, // todo remove
+    }),
+  ],
   controllers: [TasksController],
-  providers: [TasksService]
+  providers: [TasksServic],
 })
-export class TasksModule {
-}
+export class TasksModule {}
