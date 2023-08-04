091d597f292a0f61f308a2396628f79c90b72a9c
Afshin Jalili <a.jalili@kaman.ir>
2023-08-04 13:59:53 +0330

    refactor(gallatin): delete unused files

--- apps/gallatin/src/tasks/dto/update-task.dto.ts
+++
@@ -1,6 +0,0 @@
-import {PartialType} from '@nestjs/mapped-types';
-import {CreateTaskDto} from './create-task.dto';
-
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {
-    id: number;
-}
